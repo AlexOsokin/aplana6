b8ee35909c311430d482b5020cee71b231e53c50
aleksandrosokin <osomac@mail.ru>
2019-10-08 05:49:17 +0300

    Initial commit

---
+++ src/main/java/Init.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class Init {
+    private static WebDriver driver;
+    private static WebDriverWait wait;
+    //public static TestProperties props = TestProperties.getInstance();
+
+    public static void initDriver(){
+        //System.setProperty("webdriver.chrome.driver", props.getProperty("chromedriver"));
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.dns-shop.ru/");
+        wait = new WebDriverWait(driver,10);
+        //driver.manage().timeouts().implicitlyWait(Integer.parseInt(props.getProperty("timeout.global")), TimeUnit.SECONDS);
+        //driver.manage().timeouts().pageLoadTimeout(Integer.parseInt(props.getProperty("timeout.pageLoad")), TimeUnit.SECONDS);
+        //driver.get(props.getProperty("url"));
+    }
+
+    public static WebDriver getDriver(){
+        return driver;
+    }
+}
